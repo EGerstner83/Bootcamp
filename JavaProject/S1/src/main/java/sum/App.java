bda9855525376198b7ad043504aea545949a5e06
EGerstner83 <73537529+EGerstner83@users.noreply.github.com>
2021-04-07 10:12:11 -0500

    "create multiplication and division methods"

---
+++ JavaProject/S1/src/main/java/sum/App.java
@@ -0,0 +1,20 @@
+package sum;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static int multiplication(int a, int b) {
+        return (a * b);
+    }
+
+    public static double division(int a, int b) {
+        if (b == 0) {
+            return (0.0);
+        }
+        else{
+            return(a/b);
+        }
+    }
+}
